e4e07a51f1f449e3956f504ae932fc08306a059d
iedehdev <iedeh.dev@gmail.com>
2023-08-03 13:33:46 -0500

    Change sqlite to postgresql

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ import bleach
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# if os.environ.get('LOCAL'):
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# else:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
+
 db = SQLAlchemy(app)
 
 # Setup LoginManager and configure flask app to use Flask_Login
